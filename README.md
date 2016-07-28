17953efabe8d711727dff458cbeaf88a4fe528fe
java <java>
2016-07-28 17:04:23 +0900

    JSP execution confirm

--- README.md
+++ README.md
@@ -29,25 +29,39 @@
 		- >> Eclipse Gradle Import(클론한 폴더를 그래들프로젝트로..) 
 	
 	
-3. HTML
+3. HTML 설정
 	- 설정 불필요
 	- mkdir src/main/webapp 폴더생성
 	- notepad src/main/webapp/Hello.html
 	- gradle bootrun
 	- firefox ==> http://localhost:8080/Hello.html
 	
-4. JSP
+4. JSP 설정
 	- mvnrepository.com -> search&download : Tomcat embed jasper
-	- build.gradle: dependency : 
+	- build.gradle=> dependency : 
 		compile group: 'org.apache.tomcat.embed', name: 'tomcat-embed-jasper', version: '8.5.3'
-	- notepad src/main/webapp/Hello.jsp
+		compile => providedRuntime 으로 변경(배포할때 라이브러리를 빼도록 해서 tomcat 서버와의 충돌을 방지)
+		eclipse => Gradle Refresh
+	- 생성: src/main/webapp/Hello.jsp
 	- gradle bootrun
 	- firefox => http://localhost:8080/Hello.jsp
-	-
 	
-	
-	
-	
+
+5. Servlet 설정 
+	-MosaicWebAppication.java << @ServletComponentScan 추가
+	-생성: com.hybrid.servlet.HelloServlet.java 생성
+	-Project facets: 이클립스 프로젝트에 필요한 기능을 추가(자바, dynamic web module, javascript..)
+	-gradle bootrun
+	-firefox => http://localhost:8080/HelloServlet
+
+6. SpringLoaded(서블릿 리로드 기능)
+	- mvnrepository.com -> search&download : SpringLoaded
+	- build.gradle에 설정(두 군데)
+		classpath("org.springframework:springloaded:version:1.2.6.RELEASE")
+		compile group: 'org.springframework', name: 'springloaded', version: '1.2.6.RELEASE'
+	- eclipse Output directory 변경
+		bin ==> build/classes/main
+	 
 	
 	
 	 
---
+++ src/main/java/com/hybrid/servlet/HelloServlet.java
@@ -0,0 +1,56 @@
+package com.hybrid.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloServlet() {
+        super();
+        System.out.println("############");
+        System.out.println("HelloServlet");
+        System.out.println("############");
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("#######");
+        System.out.println("DoGet");
+        System.out.println("#######");
+        
+        response.setContentType("text/html; charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+       	out.println("<title>HelloServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("Method= " + request.getMethod());
+        out.println("<br>");
+        out.println("remoteAddr= " + request.getRemoteAddr());
+        for (int i = 0; i < 10; i++) {
+        	out.println("<h1>HelloServlet</h1>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+     	System.out.println("############");
+        System.out.println("DoGet");
+	    System.out.println("############");
+	}
+
+}
